554abfe4f85e09278469a0f20fee6069ad9e6a69
otsogbadrakhchinzorig <tsogbadrakh@globalsign.com>
2018-01-15 10:44:17 +0900

    To update error classes

--- src/main/java/com/walker/webwalker/service/Comparison.java
+++ src/main/java/com/walker/webwalker/service/Comparison.java
@@ -3,6 +3,8 @@ package com.walker.webwalker.service;
 import org.springframework.stereotype.Service;
 import com.walker.webwalker.util.diff_match_patch;
 
+import java.util.LinkedList;
+
 @Service
 public class Comparison {
 
@@ -12,7 +14,9 @@ public class Comparison {
         diffTool = new diff_match_patch();
     }
 
-    public toCompare(String currentElement){
-
+    public LinkedList<diff_match_patch.Diff> toCompare(String currentElement){
+        // TODO: read old data from database
+        String oldElement = "";
+        return diffTool.diff_main(oldElement, currentElement);
     }
 }
